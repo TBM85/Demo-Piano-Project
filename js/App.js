9f571c3f09d5c64a833b995e393a0116b1138a82
TBM85 <tballesterm@gmail.com>
2021-05-07 10:59:29 -0400

    Create a background for the piano

--- js/App.js
+++ js/App.js
@@ -4,84 +4,84 @@ const notes = [
     sound: "sounds/C.mp3",
     class: "white",
     value: "C",
-    letter: "(D)"
+    letter: "D"
   },
   {
     data_type: 82,
     sound: "sounds/Cb.mp3",
     class: "black",
     value: "C#",
-    letter: "(R)"
+    letter: "R"
   },
   {
     data_type: 70,
     sound: "sounds/D.mp3",
     class: "white",
     value: "D",
-    letter: "(F)"
+    letter: "F"
   },
   {
     data_type: 84,
     sound: "sounds/Db.mp3",
     class: "black",
     value: "D#",
-    letter: "(T)"
+    letter: "T"
   },
   {
     data_type: 71,
     sound: "sounds/E.mp3",
     class: "white",
     value: "E",
-    letter: "(G)"
+    letter: "G"
   },
   {
     data_type: 72,
     sound: "sounds/F.mp3",
     class: "white",
     value: "F",
-    letter: "(H)"
+    letter: "H"
   },
   {
     data_type: 85,
     sound: "sounds/Fb.mp3",
     class: "black",
     value: "F#",
-    letter: "(U)"
+    letter: "U"
   },
   {
     data_type: 74,
     sound: "sounds/G.mp3",
     class: "white",
     value: "G",
-    letter: "(J)"
+    letter: "J"
   },
   {
     data_type: 73,
     sound: "sounds/Gb.mp3",
     class: "black",
     value: "G#",
-    letter: "(I)"
+    letter: "I"
   },
   {
     data_type: 75,
     sound: "sounds/A.mp3",
     class: "white",
     value: "A",
-    letter: "(K)"
+    letter: "K"
   },
   {
     data_type: 79,
     sound: "sounds/Ab.mp3",
     class: "black",
     value: "A#",
-    letter: "(O)"
+    letter: "O"
   },
   {
     data_type: 76,
     sound: "sounds/B.mp3",
     class: "white",
     value: "B",
-    letter: "(L)"
+    letter: "L"
   }
 ];
 
@@ -95,15 +95,16 @@ const appearKeys = () => {
     keys.classList.add('key', `${note.class}`);
     keys.setAttribute('data-key', `${note.data_type}`);
 
-    const keyTitle = document.createElement('h3');
+    const keyLetter = document. createElement('h3');
+    keys.appendChild(keyLetter);
+    keyLetter.classList.add('key-letter');
+    keyLetter.innerHTML = `${note.letter}`;
+
+    const keyTitle = document.createElement('h4');
     keys.appendChild(keyTitle);
     keyTitle.classList.add('key-title');
     keyTitle.innerHTML = `${note.value}`;
 
-    const keyLetter = document. createElement('p');
-    keys.appendChild(keyLetter);
-    keyLetter.classList.add('key-letter');
-    keyLetter.innerHTML = `${note.letter}`;
 
     const audios = document.createElement('audio');
     audiosContainer.appendChild(audios);
